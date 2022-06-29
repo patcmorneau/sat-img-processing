6c14b75d7022c9c24a4d9df502692437d474438b
patrick <patrick.cmorneau@cidco.ca>
2022-06-29 12:06:10 -0400

    all resolution

--- visualize.cpp
+++ visualize.cpp
@@ -33,7 +33,8 @@ std::string extract_band_name(std::string filename){
 int main(int argc, const char* argv[]) {
     
     if (argc < 2) {
-		std::cerr << "usage: ./prosatcimg dirPath\n";
+		std::cerr << "usage: ./?? dirPath\n";
+		std::cerr << "the directory must contain all directories of the different resolution\n";
 		return -1;
 	}
 	
@@ -41,31 +42,45 @@ int main(int argc, const char* argv[]) {
 	const std::filesystem::path path{dirPath};
 	
 	std::map<std::string,std::string> files;
+	std::map<std::string,std::map<std::string,std::string>> resolutions;
 	
-	for (auto const& dir_entry : std::filesystem::directory_iterator{path}) 
-    {
-        if (dir_entry.is_regular_file()){
-			std::filesystem::path filePath = dir_entry.path();
-			files.insert({extract_band_name(filePath.filename(), dir_entry.path()});
-        }
-
-
-    }
-    
-    for(auto &el : files){
-    	std::string image = el.second;
-		std::cout<<image<<"\n";
-		
-		cv::Mat img = cv::imread(image);
-		std::cout<<img.size()<<"\n";
-		cv::resize(img, img, cv::Size(915, 915), cv::INTER_LINEAR);
-		
-		cv::imshow( "Frame", img );
-		// Press  ESC on keyboard to exit
-		char c=(char)cv::waitKey(0);
-		if(c==27) cv::destroyAllWindows();
-    }
-
+	for (auto const& dir_entry : std::filesystem::directory_iterator{path}){
+		if (dir_entry.is_directory()){
+			//std::cout<<dir_entry.path().filename()<<"\n";
+			for (auto const& file : std::filesystem::directory_iterator{dir_entry.path()}){
+				if (file.is_regular_file() && file.path().extension() == ".jp2"){
+					//std::cout<<file<<"\n";
+					std::filesystem::path filePath = file.path();
+					files.insert({extract_band_name(filePath.filename()), file.path()});
+				}
+				else{
+					std::cerr<<"not a valid file or not a .jp2 file \n";
+				}
+			}
+			resolutions.insert({dir_entry.path().filename(), files});
+			files.clear();
+		}
+	}
+	
+	for(auto &res : resolutions){
+		std::map imgs = res.second;
+		for(auto &imgPath : imgs){
+			std::string image = imgPath.second;
+			std::cout<<image<<"\n";
+			
+			cv::Mat img = cv::imread(image);
+			std::cout<<img.size()<<"\n";
+			
+			cv::resize(img, img, cv::Size(915, 915), cv::INTER_LINEAR);
+			
+			cv::imshow( res.first + "/" + imgPath.first , img );
+			// Press  ESC on keyboard to exit
+			char c=(char)cv::waitKey(0);
+			if(c==27) cv::destroyAllWindows();
+			
+		}
+	}
+	
     return 0;
 	
 }
